32e8c5b487cac3521ccd71dff1b49813af0eccc2
JOHS <upjohs@gmail.com>
2025-10-19 22:36:45 +0900

    fix: `ApiService`의 baseUrl에 프로토콜 추가
    `ApiService.kt` 파일에서 Retrofit 인스턴스를 생성할 때 사용되는 `baseUrl`에 누락된 `https://` 프로토콜을 추가합니다.
    
    - **수정 전:** `www.themealdb.com/api/json/v1/1/`
    - **수정 후:** `https://www.themealdb.com/api/json/v1/1/`
    
    이 수정은 유효한 URL 형식을 보장하여 네트워크 요청이 정상적으로 수행되도록 합니다.

--- app/src/main/java/com/beupgo/myrecipeapp/APIService.kt
+++ app/src/main/java/com/beupgo/myrecipeapp/APIService.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-private val retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/").addConverterFactory(
+private val retrofit = Retrofit.Builder().baseUrl("https://www.themealdb.com/api/json/v1/1/").addConverterFactory(
     GsonConverterFactory.create()).build()
 val recipeService = retrofit.create(ApiService::class.java)
 
